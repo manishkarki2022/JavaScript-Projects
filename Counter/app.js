72b394c61a6955d5ee0fbeadff3be2fc919c1944
manishkarki2022 <np03cs4s210031@heraldcollege.edu.np>
2023-09-01 21:42:29 +0545

    Counter app Complete Code

---
+++ Counter/app.js
@@ -0,0 +1,36 @@
+// set initial value of count
+let count = 0;
+
+const value =  document.querySelector('#value')
+
+
+
+const btns = document.querySelectorAll(".btn")
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.target.classList;
+
+        if (styles.contains("decrease")) {
+            console.log(e.target.classList)
+            // decrease the counter
+           count--;
+           
+        }else if(styles.contains("increase")){
+            count++
+        }else{
+            count = 0;
+        }
+        if(count>0){
+            value.style.color = "green"
+        }else if(count<0){
+            value.style.color = "red"
+
+        }else{
+            value.style.color ="black";
+        }
+        value.innerHTML = count
+    })
+
+})
+
